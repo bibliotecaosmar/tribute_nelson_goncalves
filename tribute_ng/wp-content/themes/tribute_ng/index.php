e6e190f4ef89217a0cbc73b0d958d5326b729f71
Osmar Pereira <bibliotecaosmar@gmail.com>
2019-07-14 11:53:57 -0300

    Fixing cover in top.

--- tribute_ng/wp-content/themes/tribute_ng/index.php
+++ tribute_ng/wp-content/themes/tribute_ng/index.php
@@ -4,49 +4,56 @@
 
 <div class="container">
 
-  <div class="row cover-mirrored"></div>
+  <div class="fixed-top">
 
-  <div class="row">
-    <div class="col-6"></div>
-    <div class="col-6 pl-5">
+    <div class="row cover-mirrored"></div>
 
-      <nav class="navbar navbar-expand-lg navbar-dark" role="navigation">
-        <div class="container">
-      	<!-- Brand and toggle get grouped for better mobile display -->
-      	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-      		<span class="navbar-toggler-icon"></span>
-      	</button>
-      		<?php
-      		wp_nav_menu( array(
-      			'theme_location'    => 'principal',
-      			'depth'             => 2,
-      			'container'         => 'div',
-      			'container_class'   => 'collapse navbar-collapse',
-      			'container_id'      => 'bs-example-navbar-collapse-1',
-      			'menu_class'        => 'nav navbar-nav',
-      			'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-      			'walker'            => new WP_Bootstrap_Navwalker(),
-      		) );
-      		?>
-      	</div>
-      </nav>
+    <div class="row">
+      <div class="col-6"></div>
+      <div class="col-6 pl-5">
 
-    </div>
-  </div>
+        <nav class="navbar navbar-expand-lg navbar-dark" role="navigation">
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <?php
+            wp_nav_menu( array(
+              'theme_location'    => 'principal',
+              'depth'             => 2,
+              'container'         => 'div',
+              'container_class'   => 'collapse navbar-collapse',
+              'container_id'      => 'bs-example-navbar-collapse-1',
+              'menu_class'        => 'nav navbar-nav',
+              'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+              'walker'            => new WP_Bootstrap_Navwalker(),
+            ) );
+            ?>
+          </div>
+        </nav>
 
-  <div class="row">
-    <div class="col-6"></div>
-    <div class="col-6 pl-5 mt-3">
-
-      <div class="card bg-transparent" style="width: 27em;">
-        <div class="card-body bg-transparent">
-          <h5 class="card-title text-light">Card title</h5>
-          <p class="card-text text-light">é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60.</p>
-          <a href="#" class="btn btn-light border border-info rounded-pill active">Go somewhere</a>
-        </div>
       </div>
-
     </div>
+
+    <div class="row">
+
+      <div class="col-6"></div>
+      <div class="col-6 pl-5 mt-3">
+
+        <div class="card bg-transparent" style="width: 27em;">
+          <div class="card-body bg-transparent">
+            <h5 class="card-title text-light">Card title</h5>
+            <p class="card-text text-light">é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60.</p>
+
+            <a href="#" class="btn btn-light border border-info rounded active">Go somewhere</a>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 
 </div>
